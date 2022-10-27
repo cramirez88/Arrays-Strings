c285c288466e88524571810cf8d275a07fea24bf
Christian Ramirez <christian.r0602@gmail.com>
2022-10-26 21:43:32 -0500

    compress

--- script.js
+++ script.js
@@ -13,12 +13,34 @@
 
 // Is prime
 
-const isPrime = (n) => {
-  if (n < 2) return false
-  for (let i = 2; i < n - 1; i++){
-    if (n % i === 0){
-      return false
+// const isPrime = (n) => {
+//   if (n < 2) return false
+//   for (let i = 2; i < n - 1; i++){
+//     if (n % i === 0){
+//       return false
+//     }
+//   }
+//   return true
+// };
+
+const compress = (s) => {
+  //   two pointers
+  let compress = ''
+  let i = 0;
+  let j = 0;
+  while (j <= s.length){
+    if(s[i] === s[j]){
+      j++
+    }else {
+      let num = j - i
+      if (num === 1) {
+        compress += s[i]
+      } else {
+        compress += num + s[i]
+      }
+       i = j
     }
+   
   }
-  return true
-};
+    return compress
+  };
